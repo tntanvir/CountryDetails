9e1fdeffa216244d1a7964688e7dfecda2fa06b5
Tanvir <tntanvir2382018@gmail.com>
2023-07-22 01:22:53 +0600

    complite problem

--- src/Component/CountryDetail.js
+++ src/Component/CountryDetail.js
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 
 const CountryDetail = () => {
     const [Country, setCountry] = useState({})
+    const [Name, setName] = useState();
+    const [Flag, setFlag] = useState()
     const { CountryName } = useParams()
     // console.log(CountryName);
 
@@ -11,23 +13,28 @@ const CountryDetail = () => {
         async function data() {
             try {
                 const dataCountry = await axios.get(`https://restcountries.com/v3.1/name/${CountryName}`);
-                // console.log(dataCountry.data);
+                console.log(dataCountry.data[0].flags.png);
                 setCountry(dataCountry.data[0])
+                setName(dataCountry.data[0]["name"]["common"])
+                setFlag(dataCountry.data[0]["flags"]["png"])
+                // setRegion(dataCountry.data[0].region)
+                console.log(dataCountry.data);
             } catch (error) {
                 console.log(error);
             }
         }
         data();
     }, []);
-    // console.log(Country);
-    const { name } = Country;
     return (
         <div>
-            {console.log(name)}
-            <li>{Country.area}</li>
-            <li>{Country.area}</li>
+            {console.log(Name, Flag)}
+            <h1>Country: {Name}</h1>
+            <img src={Flag} alt="Flag" />
+            <li>Area:{Country.area}</li>
+            <li>Region:{Country.region}</li>
+            <li>Population:{Country.population}</li>
         </div>
     );
 };
 
-export default CountryDetail;
+export default CountryDetail
